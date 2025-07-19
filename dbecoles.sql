82d301bcfd83b41df03ac7698747b0f5c7b44d01
DiabateMoussa <diabatem081@gmail.com>
2025-07-19 22:02:42 +0000

    Update dbecoles.sql

--- dbecoles.sql
+++ dbecoles.sql
@@ -155,3 +155,52 @@ alter table MOIS add constraint MOIS_pkey primary key (id_moi);
 alter table MOIS add constraint MOIS_fkey1 foreign key (id_ecol) references to ecoles on update cascade on delete cascade;
 --- ============ ennd contraint table MOIS ==================
 
+
+--- ============ table CLASSES ==================
+create table CLASSES(
+    id_cl serial,
+    nom_cl varchar(35),
+    Coef  int,
+    Niveau varchar(25), 
+    id_ecol  int    
+);
+--- ============ end table CLASSES ==================
+
+
+--- ============ contraint table CLASSES ==================
+alter table CLASSES add constraint cl_pkey primary key (id_cl);
+alter table CLASSES  add constraint not null nom_cl;
+alter table CLASSES constraint not null Coef;
+alter table CLASSES constraint cl_unique unique (nom_cl)
+alter table CLASSES constraint cl_fkey foreign key (id_ecol) references to (ECOLES) on update cascade on delete cascade;
+--- ============ ennd contraint table CLASSES ==================
+
+--- ============ table SERIES ==================
+create table SERIES(
+    id_se serial,
+    nom_se varchar(35),
+    id_ecol  int 
+);
+--- ============ contraint table SERIES ==================
+alter table SERIES add constraint se_pkey primary key (id_se);
+alter table SERIES  add constraint not null nom_se;
+alter table SERIES constraint se_unique unique (nom_se)
+alter table SERIES constraint se_fkey foreign key (id_ecol) references to (ECOLES) on update cascade on delete cascade;
+--- ============ ennd contraint table SERIES ==================
+
+create table MATIERES(
+    id_ma serial,
+    nom_ma1 varchar(50),
+    nom_ma2 varchar(50),
+    coef_ma int,
+    id_ecol  int, 
+);
+--- ============ contraint table MATIERES ==================
+alter table MATIERES add constraint ma_pkey primary key (id_ma);
+alter table MATIERES  add constraint not null nom_ma1;
+alter table MATIERES  add constraint not null nom_ma2;
+alter table MATIERES constraint ma_unique unique (nom_ma1)
+alter table MATIERES constraint ma_fkey foreign key (id_ma) references to (ECOLES) on update cascade on delete cascade;
+--- ============ ennd contraint table MATIERES ==================
+
+
