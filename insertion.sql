31873f1749b0522acfc3f7b2e5e9403155a5b4c8
DiabateMoussa <diabatem081@gmail.com>
2025-07-21 00:24:07 +0000

    Create insertion.sql

---
+++ insertion.sql
@@ -0,0 +1,7 @@
+
+insert into dg (nom_dg,prenom_dg,email_dg,tel_dg,pass_dg) values('DIAKITE','Ismail','diakites@gmail.com','76 71 13 61','azerty26');
+
+insert into ecoles(nom_ecol1,nom_ecol2,tel_ecol,id_dg) values (
+  ('META','ميتا','76 07 49 17',1),
+  ('Khomeiny','كوميني','76 60 87 89',1));
+  
